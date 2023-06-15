965881282ad9cec9195f7279265a111af43e3f16
Hujary <petzold.tom2000@gmail.com>
2023-06-15 13:57:55 +0200

    working version

--- src/components/Home_Components/Main_Component.vue
+++ src/components/Home_Components/Main_Component.vue
@@ -1,47 +1,42 @@
+
 <template>
-  <div>
-    <p>HomeView : Hello Worldcomp</p>
+  <div class="greetings">
+    <p style="background-color: rgb(61, 61, 131)">Main_Component</p>
   </div>
+
   <Generatepack_Component/>
 </template>
 
 <script>
-import axios from 'axios'
-
-
-export default {
-  name: 'ArticlesComp',
-  Component: {
-    Generatepack_Component
-  },
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      
-    };
-  },
-  mounted(){
-         //API Daten aufrufen beim lifecyle "Mounted - hinzufügen im DOM"
-      this.fetchData()
-  },
-  computed: {
-    
-  },
-  methods: {
-      // Methode für API Call
-    async fetchData(){
-      await axios.get('https://api.pokemontcg.io/v2/cards').then((response) => {
-        console.log(response)
-      }).catch(error => {
-        console.log(error)
-        this.isLoading = false
-      });
+  import axios from 'axios'
+  import Generatepack_Component from './Generatepack_Component.vue'
+  
+  export default {
+    name: 'Main_Component',
+    components: {
+      Generatepack_Component
+    },
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        
+      };
+    },
+    mounted(){
+      this.fetchData();
+    },
+    methods: {
+      async fetchData(){
+        await axios.get('https://api.pokemontcg.io/v2/cards')
+          .then((response) => {
+            console.log(response)
+          })
+          .catch(error => {
+            console.log(error)
+          });
+      }
     }
   }
-}
-</script>
-
-<style scoped>
-</style>
+  </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script>
-import Main_Component from '../components/Home_Components/Main_Component.vue'
-</script>
-
 <template>
   <main>
-    <Main_Componentt/>
+    <div>working</div>
+    <Main_Component/>
   </main>
 </template>
+
+<script>
+import Main_Component from '../components/Home_Components/Main_Component.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Main_Component
+  }
+}
+</script>
