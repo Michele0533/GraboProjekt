d77921ade4e1f26e38c7b3c949cc3c10eab37b20
Michele0533 <59345565+Michele0533@users.noreply.github.com>
2023-06-18 20:54:59 +0200

    Pack-Erstellung + Logo

--- src/components/Home_Components/Main_Component.vue
+++ src/components/Home_Components/Main_Component.vue
@@ -1,42 +1,59 @@
-
 <template>
-  <div class="greetings">
-    <p style="background-color: rgb(61, 61, 131)">Main_Component</p>
+  <div>
+    <div>
+      <img id="logo" :src="logoSrc" />
+    </div>
+    <div v-if="getapidata">
+      <Generatepack_Component :Uapidata="apidata" />
+    </div>
+    <div v-else>
+      Loading API data...
+    </div>
+    
   </div>
-
-  <Generatepack_Component/>
 </template>
 
 <script>
-  import axios from 'axios'
-  import Generatepack_Component from './Generatepack_Component.vue'
-  
-  export default {
-    name: 'Main_Component',
-    components: {
-      Generatepack_Component
-    },
-    props: {
-      msg: String
-    },
-    data() {
-      return {
-        
-      };
-    },
-    mounted(){
-      this.fetchData();
-    },
-    methods: {
-      async fetchData(){
-        await axios.get('https://api.pokemontcg.io/v2/cards')
-          .then((response) => {
-            console.log(response)
-          })
-          .catch(error => {
-            console.log(error)
-          });
+import axios from 'axios'
+import Generatepack_Component from './Generatepack_Component.vue'
+
+export default {
+  name: 'Main_Component',
+  components: {
+    Generatepack_Component,
+},
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      apidata: [],
+      getapidata: false,
+      logoSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Pokémon_Trading_Card_Game_logo.svg/1200px-Pokémon_Trading_Card_Game_logo.svg.png'
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('https://api.pokemontcg.io/v2/cards');
+        this.apidata = response.data;
+        this.getapidata = true;
+      } catch (error) {
+        console.log(error);
       }
     }
   }
-  </script>
+}
+</script>
+
+<style>
+#logo {
+  width: 25%;
+  height: 25%;
+  margin: 0 auto;
+  display: block;
+}
+</style>
