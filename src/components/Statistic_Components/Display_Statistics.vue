7d504108d730b1c4b0c5efa5f2d8cf1dd55750de
Hujary <petzold.tom2000@gmail.com>
2023-06-21 15:47:51 +0200

    added greyscale

--- src/components/Statistic_Components/Display_Statistics.vue
+++ src/components/Statistic_Components/Display_Statistics.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="StatisticView" >
+  <div class="StatisticView">
     <div>
-      {{ this.APIData }}
+      {{ ArrayAllerBisherGezogenenKartenIndexe }}
     </div>
-    <div class="image-container">
-      <img v-for="pokemon in allPokemonImages" :key="pokemon" :src="pokemon" alt="All Pokemon Card" class="pokemon-image">
+    <div class="content-container">
+      <div class="image-container">
+        <img v-for="(src, index) in allPokemonImages" :key="index" :src="src" :class="{ 'grayscale': !ArrayAllerBisherGezogenenKartenIndexe.includes(index) }" alt="All Pokemon Card" v-on:click="showImageDetails(src)">
+      </div>
+      <div class="sidebar" v-if="selectedImage">
+        <h2>Bild Details</h2>
+        <img :src="selectedImage" alt="Selected Pokemon Card" class="selected-pokemon-image">
+        <!-- Weitere Details oder Informationen zum ausgewählten Bild hier einfügen -->
+      </div>
     </div>
   </div>
 </template>
@@ -20,51 +27,73 @@ export default {
       APIData: {},
       currentuser: "",
       allPokemonCards: {},
-      allPokemonImages: []
+      allPokemonImages: [],
+      ArrayAllerBisherGezogenenKartenIndexe: [],
+      selectedImage: null
     };
   },
   mounted() {
     this.APIData = this.StatisticApidata;
     let VUEXuser = this.$store.getters.getCurrentUser;
-    let vuexApiData = this.$store.getters.getApiData;
-    this.allPokemonCards = vuexApiData.data;
+    let vuexDatenAllerPokemon = this.$store.getters.getApiData;
+    this.allPokemonCards = vuexDatenAllerPokemon.data;
 
-    // Iterate over the object
+    // Objekt aus DB in Array mit Index umwandeln
+    this.ArrayAllerBisherGezogenenKartenIndexe = this.APIData.map((obj, index) => parseInt(Object.keys(obj)[0]));
+
+    // Array, in dem nur die Bilder "SRC" gespeichert werden
     for (let pokemon in this.allPokemonCards) {
-      this.allPokemonImages[pokemon] = this.allPokemonCards[pokemon].images.large;
+      this.allPokemonImages.push(this.allPokemonCards[pokemon].images.large);
     }
 
-// Annahme: Du hast das Array mit dem Namen "data"
-
-
-// Oder Verwendung von forEach()
-/*this.APIData.forEach(obj => {
-  const index = obj.index;
-  const anzahl = obj.anzahl;
-
-  console.log(`Index: ${index}, Anzahl: ${anzahl}`);
-}); */
-
-    // Check if the user is logged in
+    // Prüfe, ob der Benutzer angemeldet ist
     if (VUEXuser == null) {
       this.currentuser = "Du musst dich anmelden, um Statistiken zu sehen.";
     } else {
       this.currentuser = VUEXuser;
     }
   },
-  methods: {}
+  methods: {
+    showImageDetails(image) {
+      this.selectedImage = image;
+    }
+  }
 }
 </script>
 
 <style scoped>
+.content-container {
+  display: flex;
+}
+
 .image-container {
+  flex: 1;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 }
 
 .pokemon-image {
-  max-width: 150px;
   margin: 10px;
+  transition: transform 0.3s ease; /* Übergangseffekt definieren */
+}
+
+.pokemon-image:hover {
+  transform: scale(1.1); /* Stiländerung bei Hover */
+}
+
+.sidebar {
+  width: 300px;
+  padding: 10px;
+  background-color: #f0f0f0;
+}
+
+.selected-pokemon-image {
+  max-width: 100%;
+  margin-bottom: 10px;
+}
+
+.grayscale {
+  filter: grayscale(100%);
 }
 </style>
